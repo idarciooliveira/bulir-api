8bd88478b6c716fc173d2017755bb15ab12a85cf
idarciooliveira <idarciooliveira@gmail.com>
2024-07-28 21:37:34 +0100

    added helmet and rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { DatabaseModule } from './infrastructure/database/database.module';
 import { HttpModule } from './infrastructure/https/https.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './infrastructure/auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 10000,
+      limit: 3,
+    }]),
     ConfigModule.forRoot({
       isGlobal: true
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet())
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api/v1');
